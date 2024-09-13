67db209a8e44df9e06df760efb9f34aa740cbd7f
SeonHyeokk <tjsgur789@gmail.com>
2024-09-13 10:22:47 +0900

    ~5

---
+++ 1_basics/4_naming_conventions.js
@@ -0,0 +1,19 @@
+/**
+ * Naming Conventions
+ * 
+ * 변수이름지을때
+ * 1) 일반적으로 영어(라틴문자)를 사용하며 문자와 숫자를 모두 사용 할 수 있다.
+ * 2) 특수기호는 언더스코어와 달러를 사용 할 수 있다.
+ * 3) 숫자로 이름을 시작 할 수 없다.
+ *  1Name,2Hello
+ * 4) 키워드는 변수명으로 사용할 수 없다.
+ *  var const = 'var';
+ */
+
+let codefactory = '코드팩토리';
+var $ive = '아이브';
+const _yuJin= '안유진';
+
+console.log(codefactory)
+console.log($ive)
+console.log(_yuJin)
